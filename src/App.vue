a1e4b73d633f055c87f96ce52e7b9952c2c371b2
Mario Morales <mario.morales@wizeline.com>
2018-12-27 18:22:38 -0600

    Updated with therapist code and logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" message="Reverse message Text"/>
+<div id="app">
+  <img alt="Eliza logo" height="120" src="./assets/logo.png" >
+  <div class="chatter">
+    <p>Hi, I'm Eliza, your online therapist. What do you want to talk about?</p>
+    <div class="controls">
+      <input type="text" placeholder="Write here" v-model="message" @keyup.enter="userMessage"/>
+      <button v-on:click="userMessage">Send</button>
+    </div>
+    <div class="messages">
+      <div class="message" v-for="message in messages" :key="message.id">
+        <p><strong>{{message.sender}}</strong>: {{message.text}} </p>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      robot: 'Eliza',
+      user: 'You',
+      messages: [],
+      message: null
+      }
+    },
+  methods: {
+    userMessage: function() {
+      this.sendMessage(this.user, this.message)
+      this.parseMessage()
+      this.message = null
+    },
+    robotMessage: function (message, timeout=400) {
+      window.setTimeout(()=>{
+        this.sendMessage(this.robot, message)
+      }, timeout);
+    },
+    sendMessage: function (sender, message) {
+      this.messages.unshift({
+        'sender': sender,
+        'text': message
+      })
+    },
+    parseMessage: function () {
+      if(this.match('what[^ ]* up') || this.match('sup') || this.match('how are you'))
+        this.robotMessage('I\'m fine thank you.')
+      if(this.match('bingo!'))
+        this.robotMessage('ding, ding, ding! We have a winner')
+    },
+    match: function (regex) {
+      return new RegExp(regex).test(this.message.toLowerCase())
+    }
   }
 }
 </script>
